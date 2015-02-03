cc41a5ec179e68d746f65217d8c99889c9758ad4
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-02-03 14:35:59 +0000

    Add implementation for K Nearest Neighbours

--- src/main/scala/smltk/Playground.scala
+++ src/main/scala/smltk/Playground.scala
@@ -1,49 +1,64 @@
 import breeze.linalg._
 import smltk.linearmodel._
+import smltk.neighbours.KNeighboursClassifier
 import smltk.metrics.ClassificationMetrics._
 
 object Playground extends App {
 
-  var X = DenseMatrix.rand[Double](200, 10)
-  val y = DenseVector.rand[Double](200)
+  // var X = DenseMatrix.rand[Double](2000, 10)
+  // val y = DenseVector((for(i <- 0 until 2000) yield scala.util.Random.nextInt(2)):_*)
+  // val y = DenseVector.rand[Double](200)
 
-  val linReg = new LinearRegression()
-  println (linReg.fit(X, y))
-  println (linReg.score(X, y))
+  val X = loadIris()
+  var y = DenseVector((for (i <- 0 until 150) yield { if(i<50) 1 else if(i<100) 2 else 3 }):_* )
+  val neigh = new KNeighboursClassifier(nNeighbours=20)
+  neigh.fit(X, y)
+  println(neigh.predict(X(0 until 150, ::)))
+  // println(neigh.predict(X))
 
-  val ridgeReg = new RidgeRegression(0.005)
-  println (ridgeReg.fit(X, y))
-  println (ridgeReg.score(X, y))
-
-  println("========== Accuracy ==========")
-  println(accuracy(DenseVector(1,2,3,4,5,6,7), DenseVector(1,2,3,4,5,6,7)))
-  println(accuracy(DenseVector(1,2,3,4,5,6,7), DenseVector(1,2,3,4,5,6,12)))
-  println(accuracy(DenseVector(1,2,3,4,5,6,7), DenseVector(1,2,3,4,5,6,12), normalize=false))
-  // println(accuracy(DenseVector(), DenseVector()))
-  val yTrue = DenseVector(1,1,1,0,0,0,1)
-  val yPreds = DenseVector(1,1,1,1,0,0,1)
-  var (tp, tn, fp, fn) = confusions(yTrue, yPreds)
-  println("******************")
-  println(accuracy(yTrue, yPreds))
-  var acc  = (tp+tn) / (tp+tn+fp+fn)
-  println(acc)
-  println("******************")
+  // def linearmodels() {
+  //   val linReg = new LinearRegression()
+  //   println (linReg.fit(X, y))
+  //   println (linReg.score(X, y))
   //
-  println("==================== CLustering ====================")
-
-  import smltk.cluster.Kmeans
-  // X = DenseMatrix.rand[Double](200, 10)
-  X = loadIris()
-  val pca = princomp(X)
-  // val model = pca.loadings(0 until 2, ::)
-  // val XNew = (model.t * model) * pca.center
-
-  val cl = new Kmeans(3, maxIterations=1000, nRuns=20)
-  cl.fit(pca.scores)
-  println(cl.centroids)
-  println(s"Objective: ${cl.objective}")
-  println(cl.labels)
-
+  //   val ridgeReg = new RidgeRegression(0.005)
+  //   println (ridgeReg.fit(X, y))
+  //   println (ridgeReg.score(X, y))
+  //
+  // }
+  // def metrics() {
+  //   println("========== Accuracy ==========")
+  //   println(accuracy(DenseVector(1,2,3,4,5,6,7), DenseVector(1,2,3,4,5,6,7)))
+  //   println(accuracy(DenseVector(1,2,3,4,5,6,7), DenseVector(1,2,3,4,5,6,12)))
+  //   println(accuracy(DenseVector(1,2,3,4,5,6,7), DenseVector(1,2,3,4,5,6,12), normalize=false))
+  //   // println(accuracy(DenseVector(), DenseVector()))
+  //   val yTrue = DenseVector(1,1,1,0,0,0,1)
+  //   val yPreds = DenseVector(1,1,1,1,0,0,1)
+  //   // var (tp, tn, fp, fn) = confusions(yTrue, yPreds)
+  //   // println("******************")
+  //   // println(accuracy(yTrue, yPreds))
+  //   // var acc  = (tp+tn) / (tp+tn+fp+fn)
+  //   // println(acc)
+  //   // println("******************")
+  //   //
+  // }
+  //
+  // def clustering() {
+  //   import smltk.cluster.Kmeans
+  //   println("==================== CLustering ====================")
+  //   // X = DenseMatrix.rand[Double](200, 10)
+  //   X = loadIris()
+  //   val pca = princomp(X)
+  //   // val model = pca.loadings(0 until 2, ::)
+  //   // val XNew = (model.t * model) * pca.center
+  //
+  //   val cl = new Kmeans(3, maxIterations=1000, nRuns=20)
+  //   cl.fit(pca.scores)
+  //   println(cl.centroids)
+  //   println(s"Objective: ${cl.objective}")
+  //   println(cl.labels)
+  // }
+  //
   def loadIris(): DenseMatrix[Double] = {
     import com.github.tototoshi.csv._
     import java.io.File
---
+++ src/main/scala/smltk/neighbours/KNeighboursClassifier.scala
@@ -0,0 +1,34 @@
+package smltk.neighbours
+
+import breeze.linalg._
+import breeze.linalg.functions.euclideanDistance
+
+class KNeighboursClassifier(val nNeighbours: Int = 5){
+  var X: DenseMatrix[Double] = _
+  var y: DenseVector[Int] = _
+  val distanceFunc = euclideanDistance
+
+  def fit(X: DenseMatrix[Double], y: DenseVector[Int]) = {
+    require(X.rows > nNeighbours, "The number of instances must be greater than K")
+    this.X = X
+    this.y = y
+  }
+
+  def predict(X: DenseMatrix[Double]): DenseVector[Int] = {
+    import scala.collection.mutable.ArrayBuffer
+    val labels = ArrayBuffer.empty[Int]
+    for(i <- 0 until X.rows) {
+      // compute distance between each point and other points
+      val distances = for(j <- 0 until X.rows) yield distanceFunc(this.X(i,::).t, X(j,::).t)
+      // take the top k distances which are the k smallest distances
+      val topKDistances = distances.sorted.take(nNeighbours)
+      // grab the indices for these distances
+      val idxs = topKDistances.map(distances indexOf)
+      // get votes for each possible label of these points
+      val freqCounts = idxs.map(i => y(i)).groupBy(n => n).map(tup => (tup._1, tup._2.size))
+      // The label with highest votes wins. TODO: what happens when there is a draw?
+      labels += freqCounts.maxBy(tup => tup._2)._1
+    }
+    DenseVector[Int](labels:_*)
+  }
+}
