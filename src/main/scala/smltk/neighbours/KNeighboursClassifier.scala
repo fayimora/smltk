4bc12962173bf115df535cc7472b6eb3dbd02a60
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-02-05 01:34:19 +0000

    Document functions in KNeighboursClassifier

--- src/main/scala/smltk/neighbours/KNeighboursClassifier.scala
+++ src/main/scala/smltk/neighbours/KNeighboursClassifier.scala
@@ -8,12 +8,23 @@ class KNeighboursClassifier(val nNeighbours: Int = 5){
   var y: DenseVector[Int] = _
   val distanceFunc = euclideanDistance
 
+  /** Fits the data. We simply just remember everything
+   *
+   * @param X The dataset
+   * @param y The corresponding labels of the dataset
+   *
+   */
   def fit(X: DenseMatrix[Double], y: DenseVector[Int]) = {
     require(X.rows > nNeighbours, "The number of instances must be greater than K")
     this.X = X
     this.y = y
   }
 
+  /** This function predicts what class the argument data point belongs to.
+   *
+   * @param x the data point for which we want to determine its class
+   *
+   */
   def predict(x: DenseVector[Double]): Int = {
     // compute distance between each point and other points
     val distances = for(i <- 0 until this.X.rows) yield distanceFunc(this.X(i, ::).t, x)
@@ -31,6 +42,11 @@ class KNeighboursClassifier(val nNeighbours: Int = 5){
     votes.maxBy(tup => tup._2)._1
   }
 
+  /** This function computes the classes of each datapoint in the argument matrix.
+   *
+   * @param X the data points
+   *
+   */
   def predict(X: DenseMatrix[Double]): DenseVector[Int] =
     X(*, ::).map( x => predict(x))
 
