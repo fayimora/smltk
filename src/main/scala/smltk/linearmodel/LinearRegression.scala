a6c98299a9a1191f4538e5e015976222e2f61d3d
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-02-09 19:53:04 +0000

    Use super class documentation

--- src/main/scala/smltk/linearmodel/LinearRegression.scala
+++ src/main/scala/smltk/linearmodel/LinearRegression.scala
@@ -5,11 +5,6 @@ import breeze.numerics._
 
 class LinearRegression extends LinearModel {
 
-  /** This functions trains the model by computing the weights.
-   *
-   * @param X the input data
-   * @param y the output vector
-   */
   def fit(X: DenseMatrix[Double], y: DenseVector[Double]): DenseVector[Double] = {
     // Setup instnce variables
     nSamples = X.rows
@@ -19,12 +14,6 @@ class LinearRegression extends LinearModel {
     weights
   }
 
-  /** Computes the pedicted output for a single instance.
-   *
-   * @param x the instance vector or which we want to comput it's output
-   *
-   * @return the predicted value for the given input instance
-   */
   def predict(x: Transpose[DenseVector[Double]]): Double = x * weights
 }
 
