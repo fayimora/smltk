5a77f1efdda2a552c8cc0afec159c8c1758e2ab0
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-02-09 19:43:52 +0000

    Add documentation for LinearRegression

--- src/main/scala/smltk/linearmodel/LinearRegression.scala
+++ src/main/scala/smltk/linearmodel/LinearRegression.scala
@@ -5,6 +5,11 @@ import breeze.numerics._
 
 class LinearRegression extends LinearModel {
 
+  /** This functions trains the model by computing the weights.
+   *
+   * @param X the input data
+   * @param y the output vector
+   */
   def fit(X: DenseMatrix[Double], y: DenseVector[Double]): DenseVector[Double] = {
     // Setup instnce variables
     nSamples = X.rows
@@ -14,11 +19,13 @@ class LinearRegression extends LinearModel {
     weights
   }
 
+  /** Computes the pedicted output for a single instance.
+   *
+   * @param x the instance vector or which we want to comput it's output
+   *
+   * @return the predicted value for the given input instance
+   */
   def predict(x: Transpose[DenseVector[Double]]): Double = x * weights
-
-  override def predict(X: DenseMatrix[Double]): DenseVector[Double] = {
-    X(*, ::).map( x => predict(x.t))
-  }
 }
 
 object LinearRegression {
