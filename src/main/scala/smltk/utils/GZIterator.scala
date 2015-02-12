866335d9658297f36ad5826a819ad76fd0a63986
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-02-12 13:27:32 +0000

    Save attempt to close gzipped files

--- src/main/scala/smltk/utils/GZIterator.scala
+++ src/main/scala/smltk/utils/GZIterator.scala
@@ -7,6 +7,12 @@ import java.util.zip.GZIPInputStream
 case class BufferedReaderIterator(reader: BufferedReader) extends Iterator[String] {
   override def hasNext() = reader.ready
   override def next() = reader.readLine()
+  // override def next() = {
+  //   val line = reader.readLine()
+  //   closeIfDone
+  //   line
+  // }
+  // def closeIfDone = if(hasNext) true else {reader.close(); false}
 }
 
 object GZIterator {
