45626af13c2548559cd3516b88d958794bee9d7c
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-02-12 20:25:17 +0000

    No point prefixing dataset loaders with 'load'

--- src/main/scala/smltk/datasets/package.scala
+++ src/main/scala/smltk/datasets/package.scala
@@ -16,7 +16,7 @@ package object datasets {
    *
    * @return a tuple of the pixels and corresponding labels
    */
-  def loadMnist(full: Boolean = true): (DenseMatrix[Double], DenseVector[Int]) = {
+  def mnist(full: Boolean = true): (DenseMatrix[Double], DenseVector[Int]) = {
     val suffix = if(full) "mnist-train.dat.gz" else "mnist123-train.dat.gz"
     val data = GZIterator(BASE_FOLDER+suffix)
     val fullMatrix = data.map(_.split("\\s+").map(_.toDouble)).toIndexedSeq
@@ -30,7 +30,7 @@ package object datasets {
    *
    * @return a tuple of the data and labels. Labels are encoded as 1,2,3
    */
-  def loadIris(): (DenseMatrix[Double], DenseVector[Int]) = {
+  def iris(): (DenseMatrix[Double], DenseVector[Int]) = {
     val data = fromFile(BASE_FOLDER+"iris.csv").getLines
     val X = data.map(l => l.split(",").dropRight(1).map(_.toDouble)).toArray
     val y = DenseVector((for (i <- 0 until 150) yield { if(i<50) 0 else if(i<100) 1 else 2 }):_* )
@@ -41,7 +41,7 @@ package object datasets {
    *
    * @return a matrix of the boston dataset
    */
-  def loadBoston(): DenseMatrix[Double] = {
+  def boston(): DenseMatrix[Double] = {
     var data = fromFile(BASE_FOLDER+"boston.dat").getLines
     val X = data.map(l => l.trim.split(" +").map(_.toDouble)).toArray
     DenseMatrix(X:_*)
