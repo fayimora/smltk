ee8c52b9301e79c31e77ff2c63e41fdcee9be4c1
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-02-12 11:44:51 +0000

    Save dimensionality of data in LogisticRegression as instance variables

--- src/main/scala/smltk/linearmodel/LogisticRegression.scala
+++ src/main/scala/smltk/linearmodel/LogisticRegression.scala
@@ -7,17 +7,21 @@ import breeze.numerics._
 
 class LogisticRegression {
   var X: DenseMatrix[Double] = _
-  // var weights: DenseMatrix[Double] = _
+  var weights: DenseMatrix[Double] = _
+  var nSamples = 0
+  var nFeats = 0
 
   def fit(X: DenseMatrix[Double], y: DenseVector[Int], k: Int) = {
     this.X = X
+    nSamples = X.rows
+    nFeats = X.cols
 
     val objective = new DiffFunction[DenseVector[Double]] {
       def calculate(thetas: DenseVector[Double]) = {
-        val w = thetas.toDenseMatrix.reshape(k, X.cols)
+        val w = thetas.toDenseMatrix.reshape(k, nFeats)
         var cost = 0.0
-        var grad = DenseVector.rand(k*X.cols)
-        for(i <- 0 until X.rows) {
+        var grad = DenseVector.rand(k*nFeats)
+        for(i <- 0 until nSamples) {
           var lhs = 0.0
           var rhs = 0.0
           for(c <- 0 until k) {
@@ -37,11 +41,11 @@ class LogisticRegression {
       }
     }
 
-    val initWeights = DenseVector.rand(k * X.cols)
+    val initWeights = DenseVector.rand(k * nFeats)
     val params = OptParams()
-    val weights = minimize(objective, initWeights, params)
+    weights = minimize(objective, initWeights, params).toDenseMatrix.reshape(k, nFeats)
     println("returned result")
-    println(weights.toDenseMatrix.reshape(3,4))
+    println(weights)
   }
 
   def predict() = {
