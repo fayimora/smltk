16be6470d78b40c03c05795b6e20184710d573a8
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-02-13 10:19:51 +0000

    [CountVectorizer] Add regex compiler and toFeatures()

--- src/main/scala/smltk/extraction/text/CountVectorizer.scala
+++ src/main/scala/smltk/extraction/text/CountVectorizer.scala
@@ -1,5 +1,20 @@
 package smltk.extraction.text
 
-class CountVectorizer {
+class CountVectorizer(val ngramRange: (Int, Int) = (1, 1),
+                      val pattern: String = "(?u)\\b\\w\\w+\\b") {
+
+  val compile = pattern.r.findAllMatchIn _
+
+  def fit() = {
+  }
+
+  def toFeatures(sequence: IndexedSeq[String]): IndexedSeq[IndexedSeq[String]] = {
+    for(seq <- sequence) yield toFeatures(seq)
+  }
+
+  def toFeatures(str: String): IndexedSeq[String] = {
+    compile(str).map(_.toString).toIndexedSeq
+  }
+
 }
 
