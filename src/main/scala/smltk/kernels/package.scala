5f6d4ebd45b7efcf159708d371c1c221c7688dc9
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-02-18 23:03:06 +0000

    correct naming

--- src/main/scala/smltk/kernels/package.scala
+++ src/main/scala/smltk/kernels/package.scala
@@ -3,9 +3,9 @@ package smltk.kernels
 import breeze.linalg._
 
 object kernels {
-  def polynomial_kernel(degree: Int)(x: DenseVector[Double], y: DenseVector[Double]) = {
+  def polynomialKernel(degree: Int)(x: DenseVector[Double], y: DenseVector[Double]) = {
   }
 
-  def gaussian_kernel(sigma: Int)(x: DenseVector[Double], y: DenseVector[Double]) = {
+  def gaussianKernel(sigma: Int)(x: DenseVector[Double], y: DenseVector[Double]) = {
   }
 }
