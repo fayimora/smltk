6a9d2cdc993f46314e21162f9ad109bdc1a9808e
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-02-18 22:35:37 +0000

    templates for polynomial_kernel() and gaussian_kernel()

---
+++ src/main/scala/smltk/kernels/package.scala
@@ -0,0 +1,13 @@
+package smltk.kernels
+
+object kernels {
+  def polynomial_kernel(degree: Int) = {
+    def withData(x: DenseVector[Double], y: DenseVector[Double]) = {
+    }
+  }
+
+  def gaussian_kernel(sigma: Int) = {
+    def withData(x: DenseVector[Double], y: DenseVector[Double]) = {
+    }
+  }
+}
