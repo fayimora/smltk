abf46bad515ad40e47c00688481fe017906d35e5
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-02-18 22:52:03 +0000

    Use currying

--- src/main/scala/smltk/kernels/package.scala
+++ src/main/scala/smltk/kernels/package.scala
@@ -1,13 +1,11 @@
 package smltk.kernels
 
+import breeze.linalg._
+
 object kernels {
-  def polynomial_kernel(degree: Int) = {
-    def withData(x: DenseVector[Double], y: DenseVector[Double]) = {
-    }
+  def polynomial_kernel(degree: Int)(x: DenseVector[Double], y: DenseVector[Double]) = {
   }
 
-  def gaussian_kernel(sigma: Int) = {
-    def withData(x: DenseVector[Double], y: DenseVector[Double]) = {
-    }
+  def gaussian_kernel(sigma: Int)(x: DenseVector[Double], y: DenseVector[Double]) = {
   }
 }
