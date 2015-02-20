8a78867f6bec35e96be00bb99af3ec68216fe200
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-02-20 00:05:22 +0000

    Add polynomial kernel implementation

--- src/main/scala/smltk/kernels/package.scala
+++ src/main/scala/smltk/kernels/package.scala
@@ -4,6 +4,7 @@ import breeze.linalg._
 
 object kernels {
   def polynomialKernel(degree: Int)(x: DenseVector[Double], y: DenseVector[Double]) = {
+    math.pow(((x dot y) + 1), degree)
   }
 
   def gaussianKernel(sigma: Int)(x: DenseVector[Double], y: DenseVector[Double]) = {
