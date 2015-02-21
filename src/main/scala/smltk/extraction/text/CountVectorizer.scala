1681f9f08603669814927b8f8b3adbead647fc25
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-02-21 23:06:53 +0000

    Refactor CountVectorizer. Add new Vectorizer trait

--- src/main/scala/smltk/extraction/text/CountVectorizer.scala
+++ src/main/scala/smltk/extraction/text/CountVectorizer.scala
@@ -2,28 +2,18 @@ package smltk.extraction.text
 
 import breeze.linalg.{Counter, DenseVector, DenseMatrix}
 
-class CountVectorizer(val ngramRange: (Int, Int) = (1, 1),
-                      val pattern: String = "(?u)\\b\\w\\w+\\b") {
+class CountVectorizer(override val ngramRange: (Int, Int) = (1, 1),
+                      val pattern: String = "(?u)\\b\\w\\w+\\b") extends Vectorizer[Int] {
 
-  val compile = pattern.r.findAllMatchIn _
+  compiler = pattern.r.findAllMatchIn _
 
-  def fit_transform(documents: IndexedSeq[String]): DenseMatrix[Int] = {
+  override def fit_transform(documents: IndexedSeq[String]): DenseMatrix[Int] = {
     val uniqueTokens = toFeatures(documents).flatten.distinct
     val mat = for(doc <- documents) yield {
       val counter = Counter.countTraversable(toFeatures(doc))
       uniqueTokens.map(tok => counter(tok))
     }
     DenseMatrix(mat:_*)
-  }
-
-  def toFeatures(sequence: IndexedSeq[String]): IndexedSeq[IndexedSeq[String]] = {
-    for(seq <- sequence) yield toFeatures(seq)
-  }
-
-  def toFeatures(str: String): IndexedSeq[String] = {
-    val tokenised = compile(str).map(_.toString).toIndexedSeq
-    val lsOfLists = for(i <- ngramRange._1 to ngramRange._2) yield tokenised.sliding(i).toIndexedSeq
-    lsOfLists.flatten.map(_.mkString(" "))
   }
 
 }
---
+++ src/main/scala/smltk/extraction/text/Vectorizer.scala
@@ -0,0 +1,32 @@
+package smltk.extraction.text
+
+import breeze.linalg.{DenseMatrix}
+
+trait Vectorizer[T] {
+
+  type Compiler = CharSequence => Iterator[scala.util.matching.Regex.Match]
+  var compiler: Compiler = _
+  val ngramRange = (1, 1)
+
+  def fit_transform(documents: IndexedSeq[String]): DenseMatrix[T]
+
+  def toFeatures(sequence: IndexedSeq[String]): IndexedSeq[IndexedSeq[String]] = {
+    for(seq <- sequence) yield toFeatures(seq)
+  }
+  // def toFeatures(sequence: IndexedSeq[String], compile: Compiler, ngramRange: (Int, Int)): IndexedSeq[IndexedSeq[String]] = {
+  //   for(seq <- sequence) yield toFeatures(seq, compile, ngramRange)
+  // }
+
+  def toFeatures(str: String): IndexedSeq[String] = {
+    val tokenised = compiler(str).map(_.toString).toIndexedSeq
+    val lsOfLists = for(i <- ngramRange._1 to ngramRange._2) yield tokenised.sliding(i).toIndexedSeq
+    lsOfLists.flatten.map(_.mkString(" "))
+  }
+  // def toFeatures(str: String, compile: Compiler, ngramRange: (Int, Int)): IndexedSeq[String] = {
+  //   val tokenised = compile(str).map(_.toString).toIndexedSeq
+  //   val lsOfLists = for(i <- ngramRange._1 to ngramRange._2) yield tokenised.sliding(i).toIndexedSeq
+  //   lsOfLists.flatten.map(_.mkString(" "))
+  // }
+
+
+}
