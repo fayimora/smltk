da74480f1cce7e541a6c824b470e3633bd98b030
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-03-13 01:06:47 +0000

    Use cleaner syntax. Code OCD :wink:

--- src/main/scala/smltk/utils/FileDownloader.scala
+++ src/main/scala/smltk/utils/FileDownloader.scala
@@ -8,9 +8,6 @@ import scala.util.{Try, Success, Failure}
 object FileDownloader {
   def apply(url: String, filename: String): Boolean = {
     val open = Try(new URL(url) #> new File(filename) !!)
-    open match {
-      case Success(v) => true
-      case Failure(e) => false
-    }
+    open map (v => true) getOrElse false
   }
 }
