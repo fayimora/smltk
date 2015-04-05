c2f7401d5779fe53771133970ee23f8c8fa16276
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-04-05 17:23:46 +0100

    Document regression metrics :wink:

--- src/main/scala/smltk/metrics/regression.scala
+++ src/main/scala/smltk/metrics/regression.scala
@@ -9,16 +9,36 @@ import breeze.numerics._
  */
 
 object regression {
+
+  /** Compute the Mean Absolute Error. This is generally used to measure how
+   *  close predictions are to eventual outcomes.
+   *  @param yTrue the true predictions
+   *  @param yPreds the model predictions
+   *
+   * @return the residual sum of squared errors
+   */
   def mae(yTrue: DenseVector[Double], yPreds: DenseVector[Double]): Double = {
     var mae = 0.0 + sum(abs(yTrue - yPreds))
     mae/yTrue.size
   }
 
+  /** Compute the Residual Sum of Squares
+   *  @param yTrue the true predictions
+   *  @param yPreds the model predictions
+   *
+   * @return the residual sum of squared errors
+   */
   def rss(yTrue: DenseVector[Double], yPreds: DenseVector[Double]): Double = {
     var rss = 0.0 + sum((yTrue - yPreds) :^ 2.0)
     rss
   }
 
+  /** Compute the Mean Squared Error. This is simply the average residual sum of squares
+   *  @param yTrue the true predictions
+   *  @param yPreds the model predictions
+   *
+   * @return the mean squared error
+   */
   def mse(yTrue: DenseVector[Double], yPreds: DenseVector[Double]): Double = {
     rss(yTrue, yPreds)/yTrue.size
   }
